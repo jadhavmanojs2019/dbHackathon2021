10c7f3d9c2b0e52431779d1dfbd2113d45c3f889
Prabhat Singh <prabhatsingh.email@gmail.com>
2021-02-26 09:44:35 +0530

    navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  showRoute = false;
   constructor() {}
+
+  onPress(event: any){
+    this.showRoute = true;
+    console.log(event);
+    // console.log('press');
+  }
 }
